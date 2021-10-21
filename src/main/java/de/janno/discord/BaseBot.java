96ba6ec4d89579dea1ecc942f827d08afab9643c
jannovs <jvs@mailbox.org>
2021-10-21 22:27:13 +0200

    keep alive optional

--- src/main/java/de/janno/discord/BaseBot.java
+++ src/main/java/de/janno/discord/BaseBot.java
@@ -7,11 +7,12 @@ public class BaseBot {
         final String token = args[0];
         final boolean updateCommands = Boolean.parseBoolean(args[1]);
         final boolean systemMetric = Boolean.parseBoolean(args[2]);
+        final boolean keepAlive = Boolean.parseBoolean(args[3]);
         Metrics.init(systemMetric);
         HttpClient httpClient = HttpClient.create()
                 .compress(true)
                 .metrics(systemMetric, s -> "")
-                .keepAlive(false) //solves some problems with connection resets on some internet connections
+                .keepAlive(keepAlive) //solves some problems with connection resets on some internet connections
                 .followRedirect(true)
                 .secure();
         new DiceSystem(httpClient, token, updateCommands);
