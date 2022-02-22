3b370a166a7555bc9dc09ef4788f76cffebe5605
jannovs <jvs@mailbox.org>
2022-02-22 23:12:16 +0100

    cleanup

--- src/test/java/de/janno/discord/command/DirectRollCommandTest.java
+++ src/test/java/de/janno/discord/command/DirectRollCommandTest.java
@@ -6,7 +6,6 @@ import de.janno.discord.api.ISlashEventAdaptor;
 import de.janno.discord.api.Requester;
 import de.janno.discord.dice.DiceParserHelper;
 import de.janno.discord.dice.IDice;
-import dev.diceroll.parser.Dice;
 import dev.diceroll.parser.NDice;
 import dev.diceroll.parser.ResultTree;
 import discord4j.core.GatewayDiscordClient;
@@ -15,17 +14,12 @@ import discord4j.core.object.command.ApplicationCommandOption;
 import discord4j.discordjson.json.ApplicationCommandInteractionOptionData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Optional;
-import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -69,13 +63,13 @@ class DirectRollCommandTest {
         verify(slashEventAdaptor).checkPermissions();
         verify(slashEventAdaptor).reply("/r expression:1d6");
         verify(slashEventAdaptor).getOption("expression");
-        verify(slashEventAdaptor,times(2)).getCommandString();
+        verify(slashEventAdaptor, times(2)).getCommandString();
         verify(slashEventAdaptor, never()).createButtonMessage(any(), any());
         verify(slashEventAdaptor, never()).deleteMessage(anyLong());
         verify(slashEventAdaptor, never()).replyEphemeral(any());
         verify(slashEventAdaptor).createResultMessageWithEventReference(eq(new Answer("1d6 = 3", "[3]", ImmutableList.of())));
 
-        verify(slashEventAdaptor,never()).getChannelId();
+        verify(slashEventAdaptor, never()).getChannelId();
     }
 
 }
