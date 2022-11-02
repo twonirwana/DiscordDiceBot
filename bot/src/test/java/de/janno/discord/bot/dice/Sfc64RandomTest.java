af72da50182317614681d0acef6080a3be5c4c90
tertu <flameshadowxeroshin@gmail.com>
2022-11-02 15:07:35 -0500

    Sfc64 update (#144)
    * Updated Sfc64 RNG implementation and DiceEvaluator compatibility
    Sfc64 is now thread-local in use
    Add code to (hopefully) reduce the overhead of that
    Add Sfc64RandomNumberSupplier
    
    * Removing unneeded Sfc64Random functions
    They were for an additional feature I ended up not adding.
    
    * Tolerate range of 1
    Fixes broken tests
    
    * use random from dice evaluator lib
    
    Co-authored-by: twonirwana <jvs@mailbox.org>

--- bot/src/test/java/de/janno/discord/bot/dice/Sfc64RandomTest.java
+++ bot/src/test/java/de/janno/discord/bot/dice/Sfc64RandomTest.java
@@ -1,5 +1,6 @@
 package de.janno.discord.bot.dice;
 
+import de.janno.evaluator.dice.random.Sfc64Random;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
