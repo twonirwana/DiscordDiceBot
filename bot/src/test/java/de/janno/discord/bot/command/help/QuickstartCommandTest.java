d26b5a8bc2b7586d190b1e4f84fc02a4d58df1c5
twonirwana <jvs@mailbox.org>
2024-03-11 22:19:04 +0100

    update /r help, improve quickstarts and add preset for Rebellion Unplugged (#467)

--- bot/src/test/java/de/janno/discord/bot/command/help/QuickstartCommandTest.java
+++ bot/src/test/java/de/janno/discord/bot/command/help/QuickstartCommandTest.java
@@ -10,6 +10,7 @@ import de.janno.discord.bot.persistance.PersistenceManager;
 import de.janno.discord.connector.api.AutoCompleteAnswer;
 import de.janno.discord.connector.api.AutoCompleteRequest;
 import de.janno.evaluator.dice.random.RandomNumberSupplier;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -55,7 +56,7 @@ class QuickstartCommandTest {
     void getPresetId_nameMatch() {
         Optional<RpgSystemCommandPreset.PresetId> res = QuickstartCommand.getPresetId("Dungeon & dragons 5e ", Locale.ENGLISH);
 
-        assertThat(res).contains(RpgSystemCommandPreset.PresetId.DND5);
+        assertThat(res).contains(RpgSystemCommandPreset.PresetId.DND5_IMAGE);
     }
 
     @Test
@@ -91,121 +92,150 @@ class QuickstartCommandTest {
 
     @Test
     void getAutoCompleteAnswer_filter() {
-        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "du", List.of()), Locale.ENGLISH, 1L,1L);
-        assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly("Dungeon & Dragons 5e",
-                "Dungeon & Dragons 5e Calculator",
-                "Dungeon & Dragons 5e Calculator 2",
-                "Dungeon & Dragons 5e with Dice Images",
-                "Dungeon Crawl Classics",
-                "Powered by the Apocalypse");
-        assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly("DND5", "DND5_CALC", "DND5_CALC2", "DND5_IMAGE", "DUNGEON_CRAWL_CLASSICS", "PBTA");
+        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "du", List.of()), Locale.ENGLISH, 1L, 1L);
+        SoftAssertions.assertSoftly(a -> {
+            a.assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly(
+                    "Dungeon & Dragons 5e",
+                    "Dungeon & Dragons 5e Calculator",
+                    "Dungeon & Dragons 5e Calculator 2",
+                    "Dungeon & Dragons 5e without Dice Images",
+                    "Dungeon Crawl Classics",
+                    "Powered by the Apocalypse");
+            a.assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly(
+                    "DND5_IMAGE",
+                    "DND5_CALC",
+                    "DND5_CALC2",
+                    "DND5",
+                    "DUNGEON_CRAWL_CLASSICS",
+                    "PBTA");
+        });
+
     }
 
     @Test
     void getAutoCompleteAnswer_filterGerman() {
-        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "du", List.of()), Locale.GERMAN, 1L,1L);
-        assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly("Dungeon & Dragons 5e",
-                "Dungeon & Dragons 5e Calculator 2",
-                "Dungeon & Dragons 5e Kalkulator",
-                "Dungeon & Dragons 5e mit Würfelbildern",
-                "Dungeon Crawl Classics",
-                "Powered by the Apocalypse",
-                "nWod / Chronicles of Darkness",
-                "oWod / Storyteller System");
-        assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly("DND5", "DND5_CALC2", "DND5_CALC", "DND5_IMAGE", "DUNGEON_CRAWL_CLASSICS", "PBTA", "NWOD", "OWOD");
+        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "du", List.of()), Locale.GERMAN, 1L, 1L);
+        SoftAssertions.assertSoftly(a -> {
+            a.assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly(
+                    "Dungeon & Dragons 5e",
+                    "Dungeon & Dragons 5e Calculator 2",
+                    "Dungeon & Dragons 5e Kalkulator",
+                    "Dungeon & Dragons 5e ohne Würfelbildern",
+                    "Dungeon Crawl Classics",
+                    "Powered by the Apocalypse",
+                    "nWod / Chronicles of Darkness",
+                    "oWod / Storyteller System");
+            a.assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly(
+                    "DND5_IMAGE",
+                    "DND5_CALC2",
+                    "DND5_CALC",
+                    "DND5",
+                    "DUNGEON_CRAWL_CLASSICS",
+                    "PBTA",
+                    "NWOD",
+                    "OWOD");
+        });
     }
 
     @Test
     void getAutoCompleteAnswer_filterBrazil() {
-        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "vampire", List.of()), Locale.of("PT", "br"), 1L,1L);
+        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "vampire", List.of()), Locale.of("PT", "br"), 1L, 1L);
         assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly("Vampiro 5ed", "nWod / Crônicas das Trevas", "oWod / Sistema Storyteller");
         assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly("VAMPIRE_5ED", "NWOD", "OWOD");
     }
 
     @Test
     void getAutoCompleteAnswer_All() {
-        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "", List.of()), Locale.ENGLISH, 1L,1L);
-        assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly("A Song of Ice and Fire",
-                "Blades in the Dark",
-                "Bluebeard's Bride",
-                "Call of Cthulhu 7th Edition",
-                "Candela Obscura",
-                "City of Mist",
-                "Coin Toss",
-                "Cyberpunk Red",
-                "Dice Calculator",
-                "Dungeon & Dragons 5e",
-                "Dungeon & Dragons 5e Calculator",
-                "Dungeon & Dragons 5e Calculator 2",
-                "Dungeon & Dragons 5e with Dice Images",
-                "Dungeon Crawl Classics",
-                "EZD6",
-                "Exalted 3ed",
-                "Fate",
-                "Fate with Dice Images",
-                "Heroes of Cerulea",
-                "Hunter 5ed",
-                "Kids on Brooms",
-                "OSR",
-                "One-Roll Engine",
-                "Paranoia: Red Clearance Edition",
-                "Powered by the Apocalypse",
-                "Prowlers & Paragons Ultimate Edition",
-                "Public Access",
-                "Risus The Anything RPG \"Evens Up\"",
-                "Rêve de Dragon",
-                "Savage Worlds",
-                "Shadowrun",
-                "Shadowrun with Dice Images",
-                "The Expanse",
-                "The Marvel Multiverse Role-Playing Game",
-                "The One Ring",
-                "Tiny D6",
-                "Traveller",
-                "Vampire 5ed",
-                "Year Zero Engine: Alien",
-                "nWod / Chronicles of Darkness",
-                "oWod / Storyteller System");
-        assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly("ASOIAF",
-                "BLADES_IN_THE_DARK",
-                "BLUEBEARD_BRIDE",
-                "CALL_OF_CTHULHU_7ED",
-                "CANDELA_OBSCURA",
-                "CITY_OF_MIST",
-                "COIN",
-                "CYBERPUNK_RED",
-                "DICE_CALCULATOR",
-                "DND5",
-                "DND5_CALC",
-                "DND5_CALC2",
-                "DND5_IMAGE",
-                "DUNGEON_CRAWL_CLASSICS",
-                "EZD6",
-                "EXALTED_3ED",
-                "FATE",
-                "FATE_IMAGE",
-                "HEROES_OF_CERULEA",
-                "HUNTER_5ED",
-                "KIDS_ON_BROOMS",
-                "OSR",
-                "ONE_ROLL_ENGINE",
-                "PARANOIA",
-                "PBTA",
-                "PROWLERS_PARAGONS",
-                "PUBLIC_ACCESS",
-                "RISUS",
-                "REVE_DE_DRAGON",
-                "SAVAGE_WORLDS",
-                "SHADOWRUN",
-                "SHADOWRUN_IMAGE",
-                "EXPANSE",
-                "MARVEL",
-                "THE_ONE_RING",
-                "TINY_D6",
-                "TRAVELLER",
-                "VAMPIRE_5ED",
-                "ALIEN",
-                "NWOD",
-                "OWOD");
+        List<AutoCompleteAnswer> res = underTest.getAutoCompleteAnswer(new AutoCompleteRequest("system", "", List.of()), Locale.ENGLISH, 1L, 1L);
+        SoftAssertions.assertSoftly(a -> {
+            a.assertThat(res.stream().map(AutoCompleteAnswer::getName)).containsExactly(
+                    "A Song of Ice and Fire",
+                    "Blades in the Dark",
+                    "Blades in the Dark without Dice Images",
+                    "Bluebeard's Bride",
+                    "Call of Cthulhu 7th Edition",
+                    "Candela Obscura",
+                    "City of Mist",
+                    "Coin Toss",
+                    "Cyberpunk Red",
+                    "Dice Calculator",
+                    "Dungeon & Dragons 5e",
+                    "Dungeon & Dragons 5e Calculator",
+                    "Dungeon & Dragons 5e Calculator 2",
+                    "Dungeon & Dragons 5e without Dice Images",
+                    "Dungeon Crawl Classics",
+                    "EZD6",
+                    "Exalted 3ed",
+                    "Fate",
+                    "Fate without Dice Images",
+                    "Heroes of Cerulea",
+                    "Hunter 5ed",
+                    "Kids on Brooms",
+                    "OSR",
+                    "One-Roll Engine",
+                    "Paranoia: Red Clearance Edition",
+                    "Powered by the Apocalypse",
+                    "Prowlers & Paragons Ultimate Edition",
+                    "Public Access",
+                    "Rebellion Unplugged",
+                    "Risus The Anything RPG \"Evens Up\"",
+                    "Rêve de Dragon",
+                    "Savage Worlds",
+                    "Shadowrun",
+                    "Shadowrun without Dice Images",
+                    "The Expanse",
+                    "The Marvel Multiverse Role-Playing Game",
+                    "The One Ring",
+                    "Tiny D6",
+                    "Traveller",
+                    "Vampire 5ed",
+                    "Year Zero Engine: Alien",
+                    "nWod / Chronicles of Darkness",
+                    "oWod / Storyteller System");
+            a.assertThat(res.stream().map(AutoCompleteAnswer::getValue)).containsExactly(
+                    "ASOIAF",
+                    "BLADES_IN_THE_DARK_IMAGE",
+                    "BLADES_IN_THE_DARK",
+                    "BLUEBEARD_BRIDE",
+                    "CALL_OF_CTHULHU_7ED",
+                    "CANDELA_OBSCURA",
+                    "CITY_OF_MIST",
+                    "COIN",
+                    "CYBERPUNK_RED",
+                    "DICE_CALCULATOR",
+                    "DND5_IMAGE",
+                    "DND5_CALC",
+                    "DND5_CALC2",
+                    "DND5",
+                    "DUNGEON_CRAWL_CLASSICS",
+                    "EZD6",
+                    "EXALTED_3ED",
+                    "FATE_IMAGE",
+                    "FATE",
+                    "HEROES_OF_CERULEA",
+                    "HUNTER_5ED",
+                    "KIDS_ON_BROOMS",
+                    "OSR",
+                    "ONE_ROLL_ENGINE",
+                    "PARANOIA",
+                    "PBTA",
+                    "PROWLERS_PARAGONS",
+                    "PUBLIC_ACCESS",
+                    "REBELLION_UNPLUGGED",
+                    "RISUS",
+                    "REVE_DE_DRAGON",
+                    "SAVAGE_WORLDS",
+                    "SHADOWRUN_IMAGE",
+                    "SHADOWRUN",
+                    "EXPANSE",
+                    "MARVEL",
+                    "THE_ONE_RING",
+                    "TINY_D6",
+                    "TRAVELLER",
+                    "VAMPIRE_5ED",
+                    "ALIEN",
+                    "NWOD",
+                    "OWOD");
+        });
     }
 }
